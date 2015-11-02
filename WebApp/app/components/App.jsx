ddf79b76c38b6aad77eadf6ffbdebd9f5a2c2659
Isaac Rose <isaac.rose@availity.com>
2015-11-02 17:56:59 -0500

    Made url generated from weather data.

--- WebApp/app/components/App.jsx
+++ WebApp/app/components/App.jsx
@@ -8,7 +8,18 @@ const OPENWEATHER_MAP_URL = 'https://mashape-community-urban-dictionary.p.mashap
 //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
 
 export default class App extends React.Component {
+
+
+
   render() {
-    return <Container url={OpenWeatherMap.url} openWeahterKey={OpenWeatherMap.key} callsign={UserConfig.callsign}/>;
-  }
+    return <Container url={this.createURL()} openWeahterKey={OpenWeatherMap.key} callsign={UserConfig.callsign}/>;
+  };
+
+  createURL(){
+  	 // url: 'http://api.openweathermap.org/data/2.5/forecast/city?' //'http://api.openweathermap.org/data/2.5/forecast/city?id={this.cityId}&APPID={this.key}'
+	return  url = OpenWeatherMap.url + 'id=' + OpenWeatherMap.cityId + '&APPID='+ OpenWeatherMap.key;
+
+  };
+
+
 };
--- WebApp/app/openWeatherMap.example.js
+++ WebApp/app/openWeatherMap.example.js
@@ -1,5 +1,5 @@
 module.exports = {
   key: 'your key goes here',
-  cityId: 'you city ID goes here', //jax
-  url: 'http://api.openweathermap.org/data/2.5/forecast/city?id={cityId}&APPID={key}'
+  cityId: 'you city ID goes here', 
+  url: 'http://api.openweathermap.org/data/2.5/forecast/city?' //ID and Key are added later.
 };
